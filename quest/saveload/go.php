1f07e8d1395acb361cb3e53199ca48e06b9c1b9e
offm2 <offmto@gmail.com>
2022-01-29 18:56:07 +0000

    Add files via upload

---
+++ quest/saveload/go.php
@@ -0,0 +1,59 @@
+<?php
+	session_start();
+//local do ficheiro
+if (isset($_SESSION["pass"])){
+$path="files/{$_SESSION["pass"]}.txt";
+}
+if($_POST["pass"]==$_SESSION["pass"]){
+//criar ficheiro e escrever
+if(isset($_SESSION["forca"])){
+
+	if(!$abrir=fopen($path,"wb")){
+		echo "Cannot open file {$path} ";
+		exit;
+	}
+//escrever conteudo no ficheiro
+	$conteudo="{$_SESSION['nome']},{$_SESSION['forca']},{$_SESSION['pericia']},{$_SESSION['sorte']},";
+	$conteudo.="{$_SESSION['provisoes']},{$_SESSION['note']},{$_SESSION['ouro']},{$_SESSION['armour']},";
+	$conteudo.="{$_SESSION['item1']},{$_SESSION['item2']},{$_SESSION['item3']},{$_SESSION['item4']},{$_SESSION['item5']},";
+	$conteudo.="{$_SESSION['item6']},{$_SESSION['item7']},{$_SESSION['item8']},{$_SESSION['item9']},{$_SESSION['item10']},";
+	$conteudo.="{$_SESSION['item11']},{$_SESSION['item12']},{$_SESSION['item13']},{$_SESSION['item14']},{$_SESSION['item15']},";
+	$conteudo.="{$_SESSION['item16']},{$_SESSION['item17']},{$_SESSION['item18']},{$_SESSION['item19']},{$_SESSION['item20']},";
+	$conteudo.="{$_SESSION['nbattle']},{$_SESSION['nbattle2']},{$_SESSION['nbattle3']},{$_SESSION['forcainicial']},{$_SESSION['pag']},";
+
+	if(fwrite($abrir,$conteudo)===FALSE){
+		echo "Cannot write to file {$path}";
+		exit;
+	}
+	echo " <h4>Saved file sucessfully...</h5>";
+	fclose($abrir);
+	
+
+
+	
+//ficheiro com a password
+/*$path2="ff.txt";
+
+	if(!$abrir2=fopen($path2,"wb")){
+		echo "Cannot open file {$path2} ";
+		exit;
+	}
+	$conteudo2=$_SESSION["pass"];
+	settype($conteudo2,"string");
+	if(fwrite($abrir2,$conteudo2)===FALSE){
+		echo "Cannot write to file {$path2}";
+		exit;
+	}
+	
+	fclose($abrir2);
+
+*/
+}
+else {echo "<h5>Session not started , could not save </h5>";}
+}
+else {echo "<h5>Password not correct</h5>";}
+
+if (isset($_SESSION["pass"])){
+echo "<h3> The password is: {$_SESSION["pass"]} </h3>Note:Please save the password in a text file for later use";
+}
+?>
