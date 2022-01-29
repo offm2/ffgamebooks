306176db61b33113e6ddf0a1e264da97039f3ed0
offm2 <offmto@gmail.com>
2022-01-29 20:14:24 +0000

    Add files via upload

--- curseof/saveload/load.php
+++ curseof/saveload/load.php
@@ -1,3 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang='en'>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Fighting Fantasy Amateur Adventure - Curse of Blackwood Manor</title>
+<link href="../../1.css" rel="stylesheet" type="text/css" />
+<link href="../../2.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<!--google translate script-->
+<script>
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({
+    pageLanguage: 'en',
+    floatPosition: google.translate.TranslateElement.FloatPosition.BOTTOM_LEFT
+  });
+}
+</script><script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<?php include_once("../../header_html/header_bootstrap.html");?>
+<?php include_once("../../menu_html/menu_bootstrap_adv2_saveload.html")?>
+<div class="container-fluid">
+<div class="row">
+  <div class="col-sm-12">
+  <h3>A Fighting Fantasy gamebooks fan site </h3>
+  <div id='rate1'></div>
+  <p>Loading adventures</p>
+    <div style="text-align:center">
+    <h3>Curse of Blackwood Manor&copy</h3><h3> by Craig Dutton  </h3>
 <?php
 	session_start();
 	ob_start();
@@ -10,7 +38,7 @@ if(ctype_alnum($_POST["pass"])){
 $path="files/{$_POST["pass"]}.txt";
 if(file_exists($path))
 {
-	
+
 //abrir e ler ficheiro
 	if(!$abrir=fopen($path,"r")){
 		echo "Cannot open file {$path} ";
@@ -47,5 +75,30 @@ $_SESSION["stime"]=time();
 $_SESSION["gamebook"]="Curse of Blackwood Manor (previously saved)";
 ?>
 Note: if the system gets to loaded older passwords will be erased
-
+<script>
+$(document).ready(function()
+{
+    $("#rater").click(function()
+    {
+        $.ajax(
+        {
+            type: "GET",
+            url: '../../rater/index.php',
+            data: {rate: 'yes'},
+            success: function(result)
+            {
+                var div = $("<div>").html(result);
+                 var section=$("#rt4",div.get(0));
+	        $("#rate1").html(section);
+            }
+        });
+    });
+	});
+</script>
+<p><img src="../../img/1.gif"></p>
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/80x15.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/">Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License</a>.
+</div>
+</div>
+</div>
+</div>
 </body>
